159ae1409f62473dc3dc7e8a26a5dfa6b8e8bdcf
vivek1729 <sasuke.vivek@gmail.com>
2017-04-23 18:59:32 +0530

    Updated README

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './store';
-import { basket as Basket }  from './basket';
+/*import { basket as Basket }  from './basket';
 
 Basket.clear();
 Basket.require({ url: 'robot_one.js', execute: false, key: 'snippet_one'})
@@ -16,7 +16,7 @@ Basket.require({ url: 'robot_one.js', execute: false, key: 'snippet_one'})
 .then(() => {
   const var2 = Basket.get('snippet_two').data;
   console.log(var2);
-});
+});*/
 /*store.subscribe(() => {
    console.log('Some event took place',store.getState());
   }
